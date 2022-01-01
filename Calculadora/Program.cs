5d828ba1ff18116ce7ac87548736551b5738d0a8
Arthur <arthurferreira@usp.br>
2022-01-01 14:42:28 -0300

    Learning C#...

---
+++ Calculadora/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Qual operação? ");
+            Console.WriteLine("1 - SOMA");
+            Console.WriteLine("2 - SUBTRAÇÃO");
+            Console.WriteLine("3 - DIVISÃO");
+            Console.WriteLine("4 - MULTIPLICAÇÃO");
+            Console.WriteLine("5 - SAIR");
+
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Selecione uma opção: ");
+
+            short opc = short.Parse(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primerio valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            // Console.WriteLine("O resultado é: " + resultado);
+            Console.WriteLine($"O resultado é {resultado}");
+            // Console.WriteLine($"O resultado é {v1 + v2}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado é {resultado}");
+            Console.ReadKey();
+            Menu();
+
+        }
+
+        static void Divisao()
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("Primeiro número: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo número: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O reultado é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("Primeiro número: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo número: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
---
+++ MeuApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int idade;
+            //int idade = 25;
+            //Console.WriteLine(idade);
+
+            //var outraIdade = 22;
+            //Console.WriteLine(outraIdade);
+
+            //var texto = "testando";
+            // Console.WriteLine(texto);
+
+            // CONSTANTE
+            //const int IDADE_MINIMA = 25;
+            //Console.WriteLine(IDADE_MINIMA);
+
+            Product mouse = new Product(1, "Mouse Gamer", 333.45, EProductType.Product);
+            // ou var mouse = new Product(1, "Mouse Gamer", 333.45);
+
+            var manutencaoEletrica = new Product(2, "Manutenção Eletrética Residencial", 300, EProductType.Service);
+
+            mouse.Id = 44;
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+            Console.WriteLine((int)mouse.Type);
+
+            Console.WriteLine(manutencaoEletrica);
+
+
+        }
+
+        struct Product
+        {
+            public Product(int id, string name, double price, EProductType type)
+            {
+                Id = id;
+                Name = name;
+                Price = price;
+                Type = type;
+            }
+
+            public int Id;
+            public string Name;
+            public double Price;
+            public EProductType Type;
+            public double PriceInDolar(double dolar)
+            {
+                return Price * dolar;
+            }
+        }
+
+        enum EProductType
+        {
+            Product = 1,
+            Service = 2
+        }
+
+    }
+}
