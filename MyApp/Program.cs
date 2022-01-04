016bc70a891103f5aa22b80d0b6132a7945b1f95
Arthur <arthurferreira@usp.br>
2022-01-04 06:53:58 -0300

    text editor'

---
+++ MyApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid(); //criar identificador global aleatório
+            //id.ToString();
+            //id = new Guid();
+            Console.WriteLine(id);
+            Console.WriteLine(id.ToString().Substring(0, 8));
+
+        }
+    }
+}
